6e14c7a4083b673b9a7b3fbb5aeaa907d759892c
Raicuparta <raicuparta@gmail.com>
2022-02-27 11:07:08 +0100

    Revert namespace block

--- ModTemplate/ModTemplate.cs
+++ ModTemplate/ModTemplate.cs
@@ -1,28 +1,30 @@
 ﻿using OWML.Common;
 using OWML.ModHelper;
 
-namespace ModTemplate;
-
-public class ModTemplate : ModBehaviour
+namespace ModTemplate
 {
-    private void Awake()
+    public class ModTemplate : ModBehaviour
     {
-        // You won't be able to access OWML's mod helper in Awake.
-        // So you probably don't want to do anything here.
-        // Use Start() instead.
-    }
-
-    private void Start()
-    {
-        // Starting here, you'll have access to OWML's mod helper.
-        ModHelper.Console.WriteLine($"My mod {nameof(ModTemplate)} is loaded!", MessageType.Success);
-
-        // Example of accessing game code.
-        LoadManager.OnCompleteSceneLoad += (scene, loadScene) =>
+        private void Awake()
         {
-            if (loadScene != OWScene.SolarSystem) return;
-            var playerBody = FindObjectOfType<PlayerBody>();
-            ModHelper.Console.WriteLine($"Found player body, and it's called {playerBody.name}!", MessageType.Success);
-        };
+            // You won't be able to access OWML's mod helper in Awake.
+            // So you probably don't want to do anything here.
+            // Use Start() instead.
+        }
+
+        private void Start()
+        {
+            // Starting here, you'll have access to OWML's mod helper.
+            ModHelper.Console.WriteLine($"My mod {nameof(ModTemplate)} is loaded!", MessageType.Success);
+
+            // Example of accessing game code.
+            LoadManager.OnCompleteSceneLoad += (scene, loadScene) =>
+            {
+                if (loadScene != OWScene.SolarSystem) return;
+                var playerBody = FindObjectOfType<PlayerBody>();
+                ModHelper.Console.WriteLine($"Found player body, and it's called {playerBody.name}!",
+                    MessageType.Success);
+            };
+        }
     }
 }
